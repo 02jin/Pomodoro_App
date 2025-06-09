642ea742027d0b7542d1f437700ad07f19648dca
heejin <sugaring12@gmail.com>
2025-06-09 13:31:55 +0900

    자투리 파일 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class PomodoroApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // 앱이 시작되면 바로 타이머 페이지를 보여줌
-      home: const TimerPage(),
+      home: TimerPage(),
     );
   }
 }
